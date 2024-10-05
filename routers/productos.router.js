fb58506bc1ff16b80159cb73b038d42d5e1a40e7
Nicolas Rocchio <nicolasrocchio92@gmail.com>
2024-10-05 18:24:23 -0300

    creo el CRUD en routers y modifico el server js

---
+++ routers/productos.router.js
@@ -0,0 +1,42 @@
+import express from "express";
+const routerProductos = express.Router();
+
+//! CRUD
+
+//! CREATE
+routerProductos.post("/", (req, res) => {
+  const producto = req.body;
+  console.log(producto);
+  res.status(201).send("Ok -> POST (CREATE)");
+});
+
+//! READ
+// GET ALL
+routerProductos.get("/", (req, res) => {
+  res.send("Ok-> GET ALL (READ)");
+});
+
+// GET ONE
+routerProductos.get("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  res.send("Ok-> GET ONE (READ)");
+});
+
+//! UPDATE
+routerProductos.put("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const productoEditado = req.body;
+  console.log(productoEditado);
+  res.send("Ok -> PUT (UPDATE)");
+});
+
+//! DELETE
+routerProductos.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  res.send("Ok-> DELETE (DELETE)");
+});
+
+export default routerProductos;
